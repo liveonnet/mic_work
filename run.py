61f00ac0775f5dd6c6dae4ad201389470c398209
liuqiang <liveonnet@qq.com>
2018-05-29 16:50:06 +0800

    记录声音的时间文本信息到stat文件

--- run.py
+++ run.py
@@ -12,6 +12,8 @@ class DailyRecording(object):
         self.p_ffmpeg = None
         self.base_dir = os.path.dirname(os.path.abspath(__file__))
         os.chdir(self.base_dir)
+        self.f_name_stat = None  # stat文件名
+        self.f_current_stat = None  # stat文件
 
     def _inRecording(self):
         return self.p_mic is not None
@@ -19,6 +21,7 @@ class DailyRecording(object):
     def _startRecording(self):
         print('start mic process...')
         f_name = '/mnt/d4/kevin/mic_data/pi_' + subprocess.check_output(shlex.split('date +%Y%m%d_%H%M%S')).decode('utf8').strip() + '.mp3'
+        self.f_name_stat = f_name + '.stat'
         self.p_mic = subprocess.Popen(shlex.split('%s ./mic.py' % sys.executable), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         self.p_ffmpeg = subprocess.Popen(shlex.split('ffmpeg -y -f s16le -ar 16000 -ac 2 -loglevel quiet -i - -acodec libmp3lame -f mp3 %s' % f_name), stdin=self.p_mic.stdout)
         print('mic process started %s %s' % (f_name, self.p_mic.stderr))
@@ -40,6 +43,10 @@ class DailyRecording(object):
             print('kill ffmpeg process done')
             self.p_ffmpeg = None
 
+        if self.f_current_stat:
+            self.f_current_stat.close()
+            self.f_current_stat = None
+
     def _statusRecording(self, timeout=5):
         s = []
         while 1:
@@ -50,7 +57,16 @@ class DailyRecording(object):
             else:
                 s.append(s_data if isinstance(s_data, str) else s_data.decode('utf8'))
         print(''.join(s), end='', file=sys.stderr, flush=True)
-        sleep(2)
+        if self.f_name_stat:
+            if self.f_current_stat:
+                if self.f_name_stat != self.f_current_stat.name:
+                    self.f_current_stat.close()
+                    self.f_current_stat = open(self.f_name_stat, 'a')
+            else:
+                self.f_current_stat = open(self.f_name_stat, 'a')
+            self.f_current_stat.write(''.join(s))
+
+        sleep(timeout)
 
     def do_work(self):
         today = None
